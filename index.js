64d40deac82b4f5f1460ba054242243b2ed554a7
Marcus <114677467+Marcusdam@users.noreply.github.com>
2023-06-25 14:27:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
- 
-const push = document.getElementById("push");
-const task = document.getElementById("task");
-
+const inputField = document.getElementById("Input");
+const Task = document.getElementById("my-task");
 
 function AddTask() {
-    if (taskInput.value == ""){
+    if (inputField.value == ""){
         alert("Please Enter Your task")
     }
     else{
         let li = document.createElement("li")
-        li.innerHTML = taskInput.value;
-        task.appendChild(li);
+        li.innerHTML = inputField.value;
+        Task.appendChild(li);
         let span = document.createElement("span")
         span.innerHTML = "\u00d7"; 
         li.appendChild(span);
     }
-    taskInput.value = "";
+    inputField.value = "";
     saveData();
    
 }
 
-task.addEventListener("click", function (e){
+Task.addEventListener("click", function (e){
     if (e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        e.target.classList.toggle("unchecked");
         saveData();
     }
 }, false);
 
 function saveData() {
-    localStorage.setItem("data", task.innerHTML);
+    localStorage.setItem("data", Task.innerHTML);
     
 }
 function showTask() {
-    task.innerHTML = localStorage.getItem("data");
-
-    
+    Task.innerHTML = localStorage.getItem("data"); 
 }
 showTask();
